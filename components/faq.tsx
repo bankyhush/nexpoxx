5fd2d6ca9ae3d1d2d2e8ed5b8c16b58846dddc0c
darkzuko315 <officialbankyhush@gmail.com>
2025-05-19 14:34:34 +0100

    TTP

--- components/faq.tsx
+++ components/faq.tsx
@@ -6,7 +6,7 @@ export default function FAQSection() {
     <section className="py-16 md:py-2 bg-white dark:bg-neutral-950">
       <div className="container mx-auto px-4">
         <h2 className="text-4xl font-semibold text-center mb-12 text-neutral-900 dark:text-white">
-          Questions? We've got answers.
+          Questions? We&rsquo;ve got answers.
         </h2>
         <div className="w-full mx-auto px-12">
           <Accordion type="single" collapsible className="w-full space-y-2">
@@ -41,7 +41,7 @@ const faqData = [
   {
     question: "Is OKX licensed and regulated in the US?",
     answer:
-      "OKX operates with various licenses and regulatory approvals in different jurisdictions. For the most up-to-date information about OKX&rsquo;s regulatory status in the US, please visit our regulatory compliance page.",
+      "OKX operates with various licenses and regulatory approvals in different jurisdictions. For the most up-to-date information about OKX is regulatory status in the US, please visit our regulatory compliance page.",
   },
   {
     question: "Can I use my bank account to fund and withdraw?",
--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import { HeroHeader } from "@/components/hero8-header";
 import { InfiniteSlider } from "@/components/ui/infinite-slider";
 import { ProgressiveBlur } from "@/components/ui/progressive-blur";
+import Image from "next/image";
 
 export default function HeroSection() {
   return (
@@ -53,7 +54,7 @@ export default function HeroSection() {
                   playsInline
                   poster="/clips/vc23-poster.jpg"
                   aria-label="Animation of the NS product in action"
-                  role="img"
+                  role="Image"
                 >
                   <source src="/clips/vc23.mp4" type="video/mp4" />
                 </video>
@@ -71,77 +72,77 @@ export default function HeroSection() {
               <div className="relative py-6 md:w-[calc(100%-11rem)]">
                 <InfiniteSlider speedOnHover={20} speed={40} gap={112}>
                   <div className="flex">
-                    <img
+                    <Image
                       className="mx-auto h-5 w-fit dark:invert"
                       src="https://html.tailus.io/blocks/customers/nvidia.svg"
                       alt="Nvidia Logo"
                       height="20"
-                      width="auto"
+                      width={100}
                     />
                   </div>
 
                   <div className="flex">
-                    <img
+                    <Image
                       className="mx-auto h-4 w-fit dark:invert"
                       src="https://html.tailus.io/blocks/customers/column.svg"
                       alt="Column Logo"
                       height="16"
-                      width="auto"
+                      width={100}
                     />
                   </div>
                   <div className="flex">
-                    <img
+                    <Image
                       className="mx-auto h-4 w-fit dark:invert"
                       src="https://html.tailus.io/blocks/customers/github.svg"
                       alt="GitHub Logo"
                       height="16"
-                      width="auto"
+                      width={100}
                     />
                   </div>
                   <div className="flex">
-                    <img
+                    <Image
                       className="mx-auto h-5 w-fit dark:invert"
                       src="https://html.tailus.io/blocks/customers/nike.svg"
                       alt="Nike Logo"
                       height="20"
-                      width="auto"
+                      width={100}
                     />
                   </div>
                   <div className="flex">
-                    <img
+                    <Image
                       className="mx-auto h-5 w-fit dark:invert"
                       src="https://html.tailus.io/blocks/customers/lemonsqueezy.svg"
                       alt="Lemon Squeezy Logo"
                       height="20"
-                      width="auto"
+                      width={100}
                     />
                   </div>
                   <div className="flex">
-                    <img
+                    <Image
                       className="mx-auto h-4 w-fit dark:invert"
                       src="https://html.tailus.io/blocks/customers/laravel.svg"
                       alt="Laravel Logo"
                       height="16"
-                      width="auto"
+                      width={100}
                     />
                   </div>
                   <div className="flex">
-                    <img
+                    <Image
                       className="mx-auto h-7 w-fit dark:invert"
                       src="https://html.tailus.io/blocks/customers/lilly.svg"
                       alt="Lilly Logo"
                       height="28"
-                      width="auto"
+                      width={100}
                     />
                   </div>
 
                   <div className="flex">
-                    <img
+                    <Image
                       className="mx-auto h-6 w-fit dark:invert"
                       src="https://html.tailus.io/blocks/customers/openai.svg"
                       alt="OpenAI Logo"
                       height="24"
-                      width="auto"
+                      width={100}
                     />
                   </div>
                 </InfiniteSlider>
